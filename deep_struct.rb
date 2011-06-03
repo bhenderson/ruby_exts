32678133e4cc3eb02ae462c5e82a9626e809b8b4
bhenderson <bhenderson@attinteractive.com>
2011-06-03 08:31:49 -0700

    clean up convert method. rewrite init method
    I originally didn't want to rewrite init, but I think ostruct should be patched to make it easier to extend.

--- deep_struct.rb
+++ deep_struct.rb
@@ -15,27 +15,54 @@
 require "ostruct"
 
 class DeepStruct < OpenStruct
-  class << self
-    def convert obj
-      case obj
-      when Hash
-        keys = obj.keys
-        values = obj.values
+  def self.convert obj
+    case obj
+    when Hash
+      keys = obj.keys
+      values = obj.values
 
-        DeepStruct.new Hash[ keys.zip( DeepStruct[values] ) ]
-      when Array
-        obj.map{|o| DeepStruct[ o ]}
-      else
-        obj
+      new Hash[ keys.zip( convert values ) ]
+    when Array
+      obj.map{|o| convert o }
+    else
+      obj
+    end
+  end
+
+  class << self
+    alias_method :[], :convert
+  end
+
+  # HACK patch for ostruct
+  def initialize(hash=nil)
+    @table = {}
+    if hash
+      for k,v in hash
+        add_member k, v
       end
     end
+  end
 
-    alias_method :[], :convert
+  def member? mem
+    @table.has_key? mem
   end
 
   def members
     @table.keys
   end
+
+  protected
+    # HACK ostruct should really implement it's own add_member
+    def modifiable
+      super
+      self
+    end
+
+    def add_member key, value
+      @table[new_ostruct_member(key)] = self.class[ value ]
+    end
+
+    alias_method :[]=, :add_member
 end
 
 class Object
--- test/test_deep_struct.rb
+++ test/test_deep_struct.rb
@@ -2,6 +2,23 @@ require "minitest/autorun"
 
 require "deep_struct"
 
+class SpecialKeys < DeepStruct
+  def new_ostruct_member key
+    key = underscore key
+    super
+  end
+
+  def underscore word
+    word = word.to_s.dup
+    word.gsub!(/::/, '/')
+    word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+    word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+    word.tr!("-", "_")
+    word.downcase!
+    word
+  end
+end
+
 class TestOpenStruct < MiniTest::Unit::TestCase
   def test_to_dstruct
     h = {:hi => "mom"}
@@ -25,5 +42,29 @@ class TestOpenStruct < MiniTest::Unit::TestCase
     ds = DeepStruct.convert( :hi => "mom" )
 
     assert_equal [:hi], ds.members
+  end
+
+  def test_new_member
+    ds = DeepStruct.convert( :hi => "mom" )
+
+    ds.foo = {:hi => "mom"}
+
+    assert_equal "mom", ds.foo.hi
+  end
+
+  def test_special_keys
+    ds = SpecialKeys.convert( :HiMom => "hello" )
+
+    assert_equal "hello", ds.hi_mom
+
+    ds.foo = {:HiMom => "hello"}
+
+    assert_equal "hello", ds.foo.hi_mom
+
+    ds.Caps = true
+
+    # I think it's unrealistic to expect ds.Caps when we want to explicitely
+    # modify other keys passed in any other way
+    refute ds.Caps
   end
 end
